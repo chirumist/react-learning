fc4f05ce6a51923eea470ab18a058b37c147b013
Chirag Mistry <cdmistry97@gmail.com>
2023-01-05 10:01:44 +0530

    -fixing console error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-const heading1 = React.createElement("h1", {}, "Hello World H1!")
-const heading2 = React.createElement("h2", {}, "Hello World H1!")
-const container = React.createElement("div", {}, [heading1, heading2])
+const cE = React.createElement
+const heading1 = cE("h1", {}, "Hello World H1!")
+const heading2 = cE("h2", {}, "Hello World H1!")
+const container = cE("div", {key: 1}, [heading1,heading2])
 
 ReactDOM.createRoot(document.querySelector('#app')).render(container)
